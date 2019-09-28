ac867b2cd99773cf828120d7583f2d1be59a894b
Andrej Ceraj <andrej.ceraj@gmail.com>
2019-09-28 14:11:30 +0200

    added last_online middleware

---
+++ app/middlewares.py
@@ -0,0 +1,21 @@
+from django.utils import timezone
+from .models import Profile
+
+
+class UpdateLastActivityMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        if request.user.is_authenticated:
+            Profile.objects.filter(user__id=request.user.id).update(last_online=timezone.now())
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
